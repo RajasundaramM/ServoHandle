16c6f148bafe4c4541bcac4d2948978682e22ac5
Rajasundaram Mathiazhagan <81680436+RajasundaramM@users.noreply.github.com>
2024-03-04 21:12:36 -0500

    Create ServoHandle.h

---
+++ ServoHandle.h
@@ -0,0 +1,17 @@
+#ifndef ServoHandle_h
+#define ServoHandle_h
+
+#include<Arduino.h>
+#include<Servo.h>
+
+class ServoHandle{
+public:
+int pin, angle, mini, maxi;
+Servo ser;
+
+ServoHandle();
+init(int p, int initangle, int minlim, int maxlim);
+pos(int ang, int del);
+};
+
+#endif
