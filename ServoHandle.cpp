d988adf7ec5c753bae1f935efa1e50195ddc8d07
Rajasundaram Mathiazhagan <81680436+RajasundaramM@users.noreply.github.com>
2024-03-04 21:13:08 -0500

    Create ServoHandle.cpp

---
+++ ServoHandle.cpp
@@ -0,0 +1,23 @@
+#include "Arduino.h"
+#include "Servo.h"
+#include "ServoHandle.h"
+
+ServoHandle :: ServoHandle(){
+
+}
+
+ServoHandle :: init(int p, int initangle, int minlim, int maxlim){
+this->pin = p;
+this->ser.attach(this->pin);
+this->mini=minlim;
+this->maxi=maxlim;
+this->pos(initangle,1000);
+}
+
+ServoHandle :: pos(int ang, int del){
+if(ang>this->maxi){ang = this->maxi;}
+if(ang<this->mini){ang = this->mini;}
+this->ser.write(ang);
+this->angle = ang;
+delay(del);
+}
